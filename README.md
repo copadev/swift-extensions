ec74c6407306ab9192f877c95a38775f149220a8
Huy Duong <minhhuy.uit@gmail.com>
2019-03-16 14:18:47 +0700

    Add UIApplication Extensions

--- README.md
+++ README.md
@@ -15,6 +15,9 @@
 - String Extensions
 - UserDefaults Extensions
 
+###### UIKit
+- UIApplication Extensions
+
 ## Requirements
 
 - iOS 10.0+ / macOS 10.12+ / tvOS 10.0+ / watchOS 3.0+
---
+++ Source UIKit/UIApplication+Extensions.swift
@@ -0,0 +1,38 @@
+//
+//  UIApplication+Extensions.swift
+//  swift-extensions
+//
+//  Created by Huy Duong on 3/15/19.
+//  Copyright © 2019 Huy Duong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: -
+
+extension UIApplication {
+    
+    /**
+     
+     */
+    class func topViewController(base: UIViewController? = (UIApplication.shared.delegate as! AppDelegate).window?.rootViewController) -> UIViewController? {
+        
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+        
+        if let tab = base as? UITabBarController {
+            if let selected = tab.selectedViewController {
+                return topViewController(base: selected)
+            }
+        }
+        
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+        
+        return base
+    }
+    
+}
